2397ef60ee81e5b529373b5ae7c6c2d3ece4244d
t.hikichi <hikichi@oscorporation.com>
2018-03-23 12:59:20 +0900

    コメントのスタイルを適用

--- comments.php
+++ comments.php
@@ -4,10 +4,10 @@ if (post_password_required()) {
 }
 ?>
 
-<div id="comments">
+<div class="comments-wrapper">
 <?php if (have_comments()) :?>
-  <h3 id="comments-count"><?php echo get_comments_number().' 件のコメント'; ?></h3>
-  <ul id="comments-list">
+  <h3 class="comments-count"><?php echo get_comments_number().' 件のコメント'; ?></h3>
+  <ul class="comments-list">
   <?php wp_list_comments(array(
       'avatar_size'=>48,
       'style'=>'ul',
@@ -16,9 +16,9 @@ if (post_password_required()) {
     )); ?>
   </ul>
 <?php if (get_comment_pages_count() > 1) : ?>
-  <ul id="comments-pagination">
-    <li id="prev-comments"><?php previous_comments_link('&lt;&lt; 前のコメント'); ?></li>
-    <li id="next-comments"><?php next_comments_link('次のコメント &gt;&gt;'); ?></li>
+  <ul class="comments-pagination">
+    <li class="prev-comments"><?php previous_comments_link('&lt;&lt; 前のコメント'); ?></li>
+    <li class="next-comments"><?php next_comments_link('次のコメント &gt;&gt;'); ?></li>
   </ul>
 <?php endif; endif; ?>
 <?php comment_form(); ?>
